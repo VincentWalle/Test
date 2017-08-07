558588e99fe9d862679893d435a228917c08bca3
xin <1229073873@qq.com>
2017-08-06 21:25:16 -0700

    test redis

---
+++ xin.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<hiredis/hiredis.h>
+using namespace std;
+
+//#define REDIS_HOST "127.0.0.1"
+//#define REDIS_PORT 6379
+const char* REDIS_HOST = "127.0.0.1";
+const int REDIS_PORT = 6379;
+
+int main()
+{
+	redisContext *c = NULL;
+	redisReply *r = NULL;
+	c = redisConnect(REDIS_HOST, REDIS_PORT);
+	if(c == NULL)
+	{
+		cout<< "connect failure" <<endl;
+		return -1;
+	}
+	cout<< "connect success" <<endl;
+	const char *cmd1 = "set test hello";
+	r = (redisReply*)redisCommand(c, cmd1);
+	if(!r)
+	{
+		cout<< "redis command error" <<endl;
+		redisFree(c);
+		return -1;
+	}
+	if(r->type == REDIS_REPLY_ERROR)
+	{
+		cout<< "redis command error" <<endl;
+		freeReplyObject(r);
+		redisFree(c);
+		return -1;
+	}
+	const char *cmd2 = "get test";
+	r = (redisReply*)redisCommand(c, cmd2);
+	if(!r)
+	{
+		cout<< "redis command error" <<endl;
+		redisFree(c);
+		return -1;
+	}
+	if(r->type == REDIS_REPLY_ERROR)
+	{
+		cout<< "redis command error" <<endl;
+		freeReplyObject(r);
+		redisFree(c);
+		return -1;
+	}
+	cout<< "get test value is:" << r->str <<endl;
+	freeReplyObject(r);
+	redisFree(c);
+	return 0; 
+}
